84e234a6fb31e9afa66989ae1d5644998bc8c618
Njugunaevans <114911702+Njugunaevans@users.noreply.github.com>
2024-02-13 18:50:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const servicesButtons=document.querySelectorAll(".service_item");
-const  serviceDetails=document.querySelectorAll(".services_right");
 
-const getservice=(category)=>{
-    const details=servicedata.find(item => item.category===category);
-    serviceDetails.innerHTML= <h3>${details.title}
-    {details.description.map(paragraph => "<p>" + paragraph+ "</p>").join("")} </h3>
-    
-}
-const removeActiveClass=() =>{
-    servicesButtons.forEach(button => {
-        button.classList.remove('active');
-    });
-}
-servicesButtons.forEach(item=> {
-    item.addEventListener("click",() =>{
-        removeActiveClass();
-      const serviceclass=item.classList[1]  
-       getservice(serviceclass)
-       item.classList.add('active')
-    })
-})
-getservice('frontend')
- 
+
+    var tablinks=document.getElementsByClassName("tab-links");
+    var tabcontents=document.getElementsByClassName("tab-contents");
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classlist.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabbcontent.classlist.remove("active-tab");
+        }
+        
+Event.currentTarget.classlist.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
+    }
+
+
